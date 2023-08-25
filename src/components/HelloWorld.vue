cc44d79def6c0a28c87e5d9c0bdf5257269c4036
YudzhinNSK <spidi2e@mail.ru>
2023-08-25 13:43:43 +0400

    add template for vue3 + ts + pinia

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="test">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './assets/stylesheets/application.scss'
+import App from './App.vue'
+
+createApp(App).mount('#app')
